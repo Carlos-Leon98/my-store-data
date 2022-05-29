606a185eefcd694ab540248fa540b37894fed479
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2022-05-29 09:17:41 -0500

    Setting up and running migrations with npm scripts

---
+++ db/migrations/20220529140406-create-user.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { UserSchema, USER_TABLE } = require("../models/user.models")
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+  },
+
+  async down (queryInterface) {
+    await queryInterface.drop(USER_TABLE);
+  }
+};
