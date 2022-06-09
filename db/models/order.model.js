e134e23b238f992c3e00f6639389c3fd640d9d09
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2022-06-09 09:15:54 -0500

    Relations many-to-many

--- db/models/order.model.js
+++ db/models/order.model.js
@@ -35,6 +35,12 @@ class Order extends Model {
     this.belongsTo(models.Customer, {
       as: 'customer'
     });
+    this.belongsToMany(models.Product, {
+      as: 'items',
+      through: models.OrderProduct,
+      foreignKey: 'orderId',
+      otherKey: 'productId'
+    })
   }
 
   static config(sequelize) {
