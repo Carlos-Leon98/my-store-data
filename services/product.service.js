b516237f4b1cae462203aba3f767314699368ccb
Carlos-Leon98 <carlosaleonrivas@gmail.com>
2022-06-11 15:09:12 -0500

    Query part02

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const { Op } = require('sequelize');
 const boom = require('@hapi/boom');
 
 const { models } = require('../lib/sequelize');
@@ -30,12 +31,24 @@ class ProductsService {
 
   async find(query) {
     const options = {
-      include: ['category']
+      include: ['category'],
+      where: {}
     };
     const { limit, offset } = query;
     if (limit && offset) {
       options.limit = limit;
       options.offset = offset;
+    };
+    const { price } = query;
+    if (query) {
+      options.where.price = price;
+    };
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max
+      };
     }
     const products = await models.Product.findAll(options);
     return products;
